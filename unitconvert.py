0092913b2d1515474b0d1b0a12e9c9dbbac4de53
mocha <mocha2007andcoco@gmail.com>
2017-04-04 20:24:05 -0400

    Update unitconvert.py

--- unitconvert.py
+++ unitconvert.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python
 #Mocha's unit conversion suite
+#velocity
+from math import pi
 def mih2ms(v):
     return n*1397/3125
 def mih2kmh(v):
     return n*50292/3125
+#length
 def mi2m(l):
     return n*201168/125
 def mi2km(l):
     return n*50292/3125
+def au2m(l):
+    return n*149597870700
+def ly2m(l):
+    return n*9460730472580800
+def pc2m(l):
+    return au2m(n*648000/pi())
+#time
 def y2s(t):
     return n*31556952
+#pressure
 def atm2pa(p):
     return n*101325
+#temperature
 def f2k(t):
     return (n+459.67)*5/9
 def f2c(t):
