d17366ab2490b1ec1a8d981a1356aec97faf66f8
mocha <mocha2007andcoco@gmail.com>
2017-04-04 20:13:33 -0400

    more conversions
    huzzah!

--- unitconvert.py
+++ unitconvert.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
+#Mocha's unit conversion suite
 def mih2ms(v):
     return n*1397/3125
+def mih2kmh(v):
+    return n*50292/3125
 def mi2m(l):
     return n*201168/125
 def mi2km(l):
     return n*50292/3125
+def y2s(t):
+    return n*31556952
+def atm2pa(p):
+    return n*101325
 while 1:
     print(input("Use a function: "))
