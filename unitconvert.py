62f20716bb92e1ce8c2b9f42bce37c3362625768
mocha <mocha2007andcoco@gmail.com>
2017-04-04 20:08:26 -0400

    Add files via upload

---
+++ unitconvert.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+def mih2ms(v):
+    return n*1397/3125
+def mi2m(l):
+    return n*201168/125
+def mi2km(l):
+    return n*50292/3125
+while 1:
+    print(input("Use a function: "))
